9f30b7a6e7110e78f5a5a41544db1b15ca6e1b7a
Siddhant Medar <sidmedar21@gmail.com>
2025-04-06 14:38:47 -0500

    add bernoulli nb

--- algorithms-from-scratch/classical_ml/naive_bayes/bernoulli_nb.py
+++ algorithms-from-scratch/classical_ml/naive_bayes/bernoulli_nb.py
@@ -5,7 +5,7 @@ np.random.seed(313)
 
 
 n_samples, n_features = 1000, 3
-X = np.random.randn(n_samples, n_features)
+X = np.random.randint(0,2,(n_samples, n_features))
 y = np.random.randint(0, 2, n_samples)
 
 split_ratio = int(0.90 * n_samples)
@@ -14,13 +14,12 @@ X_train, y_train = X[:split_ratio], y[:split_ratio]
 X_test, y_test = X[split_ratio:], y[split_ratio:]
 
 
-class GaussianNaiveBayes:
-    def __init__(self):
+class BernoulliNaiveBayes:
+    def __init__(self, alpha=1):
         self.prior_probs = None
         self.classes = None
         self.n_features = None
-        self.mean = None
-        self.var = None
+        self.alpha = alpha
 
     def fit(self, X, y):
         self.n_features = X.shape[1]
@@ -28,16 +27,14 @@ class GaussianNaiveBayes:
 
         # compute prior probabilities
         self.prior_probs = np.bincount(y) / len(y)
-        
-        # compute conditional probabilities
-        self.mean = np.zeros((len(self.classes),self.n_features))
-        self.var = np.zeros((len(self.classes),self.n_features))
+        self.conditional_probs = np.zeros((len(self.classes), self.n_features))
 
         for c in self.classes:
             class_points = X[y == c]
-            self.mean[c] = np.mean(class_points,axis=0)
-            self.var[c] = np.var(class_points,axis=0)
-
+            p_c = (class_points.sum(axis=0) + self.alpha) / (
+                len(class_points) + self.alpha * 2
+            )
+            self.conditional_probs[c] = p_c + 1e-9
     def predict(self, X):
         y_pred = []
 
@@ -46,7 +43,10 @@ class GaussianNaiveBayes:
 
             for c in self.classes:
                 log_prior = np.log(self.prior_probs[c])
-                log_likelihood = -0.5 * np.sum(np.log((2*np.pi*self.var[c])) + (x-self.mean[c])**2 / self.var[c])
+                log_likelihood = np.sum(
+                    x * np.log(self.conditional_probs[c])
+                    + (1 - x) * np.log(1-self.conditional_probs[c]))
+                
                 log_probs.append(log_prior + log_likelihood)
 
             y_pred.append(np.argmax(log_probs))
@@ -54,13 +54,14 @@ class GaussianNaiveBayes:
         return np.array(y_pred)
 
 
-def compute_accuracy(y_true,y_pred):
+def compute_accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
 
 
 if __name__ == "__main__":
-    nb = GaussianNaiveBayes()
+    nb = BernoulliNaiveBayes()
     nb.fit(X_train, y_train)
+    print(nb.conditional_probs)
     y_pred = nb.predict(X_test)
 
     print("Accuracy:", compute_accuracy(y_test, y_pred))
